d1ec41b00d584c22944651a9c0453aa07e6f2416
William Lahti <wilahti@gmail.com>
2022-03-04 19:14:09 -0800

    documentation

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,8 +1,26 @@
 
+/**
+ * Opt into dependency injection when requireOptIn is true.
+ * Also required when not using emitDecoratorMetadata (instead of a compatible metadata transformer)
+ * @returns 
+ */
 export function Injectable() {
-    return target => {};
+    return Reflect.metadata('pdi:injectable', true);
 }
 
+/**
+ * Mark an injection (constructor parameter, property) as optional. If the injector cannot find a suitable 
+ * provider, `undefined` will be supplied instead.
+ * @returns 
+ */
 export function Optional() {
-    return (target, propertyKey?) => {};
+    return Reflect.metadata('pdi:optional', true);
+}
+
+/**
+ * Inject into a constructor parameter or property
+ * @param token The dependency injection token. If not provided, the type of the parameter/property is used instead.
+ */
+export function Inject(token? : any) {
+    return Reflect.metadata('pdi:inject', token);
 }
--- src/injector.ts
+++ src/injector.ts
@@ -1,14 +1,27 @@
-import { reflect, ReflectedTypeRef } from 'typescript-rtti';
+import { InterfaceToken, reflect, ReflectedTypeRef } from 'typescript-rtti';
 import { Constructor } from './common';
 export interface Dependency<T = any> { tokens : any[]; optional? : boolean; default? : () => T; }
+
+export type Provider = (...args) => any;
+
+/**
+ * Represents a dependency injector.
+ */
 export class Injector {
+    /**
+     * Construct an injector with the given providers and parent injector. Once constructed the 
+     * injector will be able to provide any of the dependencies specified by the providers by using 
+     * provide(), construct(), or invoke().
+     * @param providers 
+     * @param parent 
+     */
     constructor(providers : [any, Function][], parent? : Injector) {
         this.#providers = new Map(providers);
         this.#providers.set(Injector, () => this);
         this.#parent = parent;
     }
 
-    static construct(constructor : Constructor): Function {
+    static construct(constructor : Constructor): Provider {
         return (i : Injector) => i.construct(constructor);
     }
 
@@ -16,7 +29,16 @@ export class Injector {
     #providers = new Map<any, Function>();
     #resolved = new Map<any, any>();
 
+    /**
+     * Provide an instance for the given class. 
+     * @param constructor 
+     */
     provide<T>(constructor : Constructor<T>): T;
+    /**
+     * Provide an object that implements the given interface via a token that represents it.
+     * @param token 
+     */
+    provide<T>(token : InterfaceToken): any;
     provide(token : any): any {
         if (this.#resolved.has(token))
             return this.#resolved.get(token);
@@ -29,18 +51,41 @@ export class Injector {
         return resolved;
     }
 
+    /**
+     * Invoke the given function, providing its arguments using the injector.
+     * @param func 
+     * @returns The result of the function
+     */
     invoke<T = any>(func : ((...args) => T) | Function): T {
         return <T> func(...this.analyze(func).map(d => this.resolve(d)));
     }
 
+    /**
+     * Construct the given class by using the injector to resolve and provide its dependencies.
+     * This creates a new instance of the class every time, and does not resolve the class itself 
+     * using the providers of this injector.
+     * 
+     * @param klass 
+     * @returns 
+     */
     construct<T>(klass : Constructor<T>): T {
         return new klass(...this.analyze(klass).map(d => this.resolve(d)));
     }
 
+    /**
+     * Resolve the given dependency. This is the core mechanism for providing dependencies internally.
+     * @param dep 
+     * @returns 
+     */
     private resolve(dep : Dependency) {
         return this.invoke(() => dep.tokens.map(t => this.provide(t)).filter(x => x)[0]);
     }
 
+    /**
+     * Analyze the given function (or constructor) to determine its dependencies.
+     * @param ctor 
+     * @returns 
+     */
     private analyze<T>(ctor : Constructor<T> | Function): Dependency[] {
         return reflect(ctor).metadata('pdi:deps', () => {
             return Object.seal(reflect(ctor).parameters.map(p => ({ 
@@ -51,6 +96,11 @@ export class Injector {
         });
     }
 
+    /**
+     * Produce a Dependency that corresponds to the given runtime type reference.
+     * @param typeRef 
+     * @returns 
+     */
     private dependency(typeRef : ReflectedTypeRef) : Dependency {
         if (typeRef.isUnion())
             return { tokens: typeRef.types.map(t => this.dependency(t)).map(d => d.tokens).flat() };
@@ -63,11 +113,34 @@ export class Injector {
     }
 }
 
+/**
+ * Create a new injector with the given providers and parent injector. 
+ * This is a function-oriented alternative to `new Injector(providers, parent)`
+ * 
+ * @param providers 
+ * @param parent 
+ * @returns 
+ */
 export function injector(providers : [any, Function][], parent? : Injector): Injector {
     return new Injector(providers, parent);
 }
 
-export function construct(constructor : Constructor) { return Injector.construct(constructor); }
+/**
+ * Creates a dependency injection provider which constructs the given class.
+ * @param constructor 
+ * @returns 
+ */
+export function construct(constructor : Constructor): Provider { return Injector.construct(constructor); }
+
+/**
+ * Creates a dependency injection provider for the given constructor. When an injection calls for the 
+ * constructor as its token or when the type specified at the injection site is the class represented by the 
+ * constructor, this provider will be used.
+ * 
+ * @param constructor The class constructor to provide. It will be provided by constructing it. All constructor
+ *                    parameters will be provided by the dependency injector.
+ * @returns 
+ */
 export function provide<T>(constructor : Constructor<T>): [ Function, Function ] {
     return [constructor, construct(constructor)];
 }
